1e7dc05c6d4f6e05c34beb38fb656eaebde53bc3
Sean Glendinning <allcdnboy@gmail.com>
2023-09-06 14:45:14 -0700

    return Response

--- src/Resources/Customers.php
+++ src/Resources/Customers.php
@@ -2,21 +2,22 @@
 
 namespace Textline\Resources;
 
+use Textline\Http\Response;
+
 class Customers extends Resource
 {
     public function get(array $query = [])
     {
         return $this->client
-                         ->get('api/customers.json', $query)
-                         ->getContent();
+            ->get('api/customers.json', $query)
+            ->getContent();
     }
 
-    public function create(string $number, $body = [])
+    public function create(string $number, $body = []): Response
     {
         return $this->client
-                         ->post('api/customers.json', ['customer' => array_merge($body, [
-                             'phone_number' => $number
-                         ])])
-                         ->getContent();
+            ->post('api/customers.json', ['customer' => array_merge($body, [
+                'phone_number' => $number
+            ])]);
     }
 }
